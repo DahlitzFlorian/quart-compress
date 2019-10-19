81a5a58e8d01546d8475de5cbb2de56f11da0dc8
Florian Dahlitz <f2dahlitz@freenet.de>
2019-10-19 19:55:55 +0200

    feat(tests): test DictCache methods

--- tests/integration/test_caching.py
+++ tests/integration/test_caching.py
@@ -5,6 +5,18 @@ from integration.helper import _client_get
 
 
 @pytest.mark.asyncio()
+async def test_caching_methods(setup):
+    app = setup
+    app.config["COMPRESS_CACHE_BACKEND"] = DictCache()
+    app.config["COMPRESS_CACHE_BACKEND"].set("cache", "Ipsum Lorem")
+    app.config["COMPRESS_CACHE_KEY"] = "cache"
+
+    cache_data = app.config["COMPRESS_CACHE_BACKEND"].get("cache")
+
+    assert cache_data == "Ipsum Lorem"
+
+
+@pytest.mark.asyncio()
 async def test_caching_basic(setup_with_sizes):
     app, small_size, large_size = setup_with_sizes
     app.config["COMPRESS_CACHE_BACKEND"] = DictCache()
@@ -13,4 +25,5 @@ async def test_caching_basic(setup_with_sizes):
 
     response = await _client_get(app, "/small")
     data = await response.get_data()
+
     assert small_size == len(data)
